bdee8bb74cfb0327c43432d0f38c08373c3bcd19
OLIVIAMANDOLA <ovm5126@psu.edu>
2020-08-25 22:04:46 +0000

    more complicated way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 name = input("Enter your name: ")
-print(f"Hello, {name}!")
-age =  input("Ener your age: ")
+print(f"Hello," + name + "!")
+age =  input("Enter your age: ")
 age = int(age)
-print(f"You will be {age + 1} next year.")
+print("You will be " + str(age + 1) + " next year.")
